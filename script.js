9a3b23219b8bc8ef47dfd8514c968f2f0a4134df
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-12-14 09:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your code here
 const mainDiv = document.getElementById('app');
+const button = document.getElementById('swap');
 
 function swapTheme() {
 	if (mainDiv.className == 'day'){
 		mainDiv.className = 'night';
+		button.className = 'button_night';
 	}
 	else {
 		mainDiv.className = 'day';
+		button.className = 'button_day';
 	}
 }
+button.addEventListener('click', swapTheme);
