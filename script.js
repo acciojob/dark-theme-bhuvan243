d770be5ac0687b6e5f073587b1b64785c62d2f66
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-12-14 09:22:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+const mainDiv = document.getElementById('app');
 
+function swapTheme() {
+	if (mainDiv.className == 'day'){
+		mainDiv.className = 'night';
+	}
+	else {
+		mainDiv.className = 'day';
+	}
+}
